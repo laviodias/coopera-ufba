e95652a90c670da6b88a30ace109124a5a37d308
Lávio Vale <laviovale@gmail.com>
2025-04-05 14:26:01 -0300

    Descricao pode ser null

--- apps/api/prisma/migrations/20250405120545_add_project_description/migration.sql
+++ apps/api/prisma/migrations/20250405120545_add_project_description/migration.sql
@@ -3,11 +3,10 @@
 
   - A unique constraint covering the columns `[name]` on the table `Keyword` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[name]` on the table `KnowledgeArea` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `description` to the `Project` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "description" TEXT NOT NULL;
+ALTER TABLE "Project" ADD COLUMN     "description" TEXT;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Keyword_name_key" ON "Keyword"("name");
