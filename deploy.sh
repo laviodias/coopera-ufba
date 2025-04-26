dae6b0544e2779375035ae879579b64bd5c821d4
Lávio Vale <laviovale@gmail.com>
2025-04-26 19:54:05 -0300

    Atualizando frontend

--- deploy.sh
+++ deploy.sh
@@ -4,39 +4,48 @@ set -e
 
 # Configurações
 REMOTE_USER=ubuntu
-REMOTE_HOST=100.24.7.198
+REMOTE_HOST=18.208.211.41
 REMOTE_PATH=/home/ubuntu
 SSH_KEY_PATH=~/Downloads/lavio-coopera.pem
 
 # Nomes
-BACKEND_IMAGE_NAME=coopera-backend
 FRONTEND_IMAGE_NAME=coopera-frontend
 IMAGE_TAG=prod
-BACKEND_TAR=backend-image.tar
 FRONTEND_TAR=frontend-image.tar
 
-echo "🚧 Buildando imagens localmente..."
+echo "🚧 Buildando imagem do frontend localmente..."
 
-docker build -t $BACKEND_IMAGE_NAME:$IMAGE_TAG -f apps/api/Dockerfile apps/api
+# Build da imagem do frontend
 docker build -t $FRONTEND_IMAGE_NAME:$IMAGE_TAG -f apps/web/Dockerfile apps/web
 
-echo "📦 Salvando imagens em .tar..."
-docker save -o $BACKEND_TAR $BACKEND_IMAGE_NAME:$IMAGE_TAG
+echo "📦 Salvando imagem do frontend em .tar..."
 docker save -o $FRONTEND_TAR $FRONTEND_IMAGE_NAME:$IMAGE_TAG
 
-echo "🚀 Enviando imagens para EC2..."
-scp -i "$SSH_KEY_PATH" $BACKEND_TAR $FRONTEND_TAR $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+echo "🚀 Enviando alterações para o repositório Git..."
 
-echo "📂 Carregando imagens e subindo containers na EC2..."
+# Enviando as alterações para o Git
+git add .
+git commit -m "Atualizando frontend"
+git push origin main
+
+echo "📂 Entrando na instância EC2 e atualizando o repositório..."
+
 ssh -i "$SSH_KEY_PATH" $REMOTE_USER@$REMOTE_HOST <<'EOF'
   set -e
-  cd /home/ubuntu
-  docker load -i backend-image.tar
+
+  # Entrando no diretório do repositório
+  cd coopera-ufba
+
+  # Atualizando o repositório com o git pull
+  git pull origin main
+
+  echo "🚀 Carregando a imagem do frontend..."
   docker load -i frontend-image.tar
+
+  echo "🚀 Subindo containers..."
   docker-compose -f docker-compose.prod.yml down
+  docker-compose -f docker-compose.prod.yml build
   docker-compose -f docker-compose.prod.yml up -d
-  rm backend-image.tar frontend-image.tar
 EOF
 
-echo "✅ Deploy concluído!"
-#rm $BACKEND_TAR $FRONTEND_TAR
+echo "✅ Processo concluído com sucesso!"
