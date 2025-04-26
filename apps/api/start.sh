e26f75311a6bb36a01fdb045471ac5530a94cfdf
Lávio Vale <laviovale@gmail.com>
2025-04-26 20:27:45 -0300

    fix start script

--- apps/api/start.sh
+++ apps/api/start.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 # Função para verificar se o banco de dados está acessível
 wait_for_db() {
+  echo "Aguardando banco de dados..."
   until nc -z -v -w30 db 5432; do
     echo "Aguardando banco de dados..."
     sleep 1
   done
-  echo "Banco de dados disponível!"
 }
 
 echo "Aplicando migrações do Prisma..."
@@ -18,19 +18,4 @@ if ! npx prisma migrate deploy; then
   exit 1
 fi
 
-# Criar diretório para o arquivo de controle se necessário
-mkdir -p $(dirname ./.seeds-executed)
-
-if [ ! -f ./.seeds-executed ]; then
-  echo "Executando seeds do banco de dados..."
-  if ! node dist/prisma/seed.js; then
-    echo "Erro ao executar seeds. Abortando."
-    exit 1
-  fi
-  touch ./.seeds-executed
-else
-  echo "Seeds já foram executados anteriormente."
-fi
-
-echo "Iniciando a aplicação..."
-exec npm run start:prod
+# Restante do seu script...
